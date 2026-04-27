8f50531bcd5fac5d07075c82d0629ccb4d410cc9
khuslenshi <kkhuslenshi@gmail.com>
2026-04-27 19:33:24 +0800

    fix: clean up routing structure and remove duplicate files

--- partB/src/routes/bookRoutes.js
+++ partB/src/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/bookController");
-const apiKeyAuth = require("../middle/apiKeyAuth");
+const apiKeyAuth = require("../middleware/apiKeyAuth");
 
 
 router.get("/books", controller.getAllBooks);
